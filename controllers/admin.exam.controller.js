667bc249cd1d49f050d4466767e0e88ec671f462
Dawn Nguyen <55096320+DawnHNguyen@users.noreply.github.com>
2024-04-21 22:26:49 +0700

    #4 feat: api analysis for exam result (#8)

--- controllers/admin.exam.controller.js
+++ controllers/admin.exam.controller.js
@@ -78,7 +78,113 @@ const adminExamController = {
         } catch (error) {
             res.status(500).send({ message: error.message });
         }
+    },
+
+    listExamResults: async (req, res) => {
+        try { 
+            const userId = req.query.userId;
+            const examId = req.query.examId;
+            if (userId) await listExamResultsByUserId(req, res);
+            else if (examId) await listExamResultsByExamId(req, res);
+            else res.status(400).send({ message: 'User ID or Exam ID is required' });
+        } catch (error) {
+            res.status(500).send({ message: error.message });
+        }
     }
 };
 
 module.exports = adminExamController;
+
+async function listExamResultsByUserId(req, res) {
+    try {
+        const userId = req.query.userId;
+        const {name, type, submittedAtFrom, submittedAtTo} = req.query;
+
+        const {db, client} = await connectDb();
+        const examResultsCollection = db.collection(constants.EXAM_RESULTS_COLLECTION_NAME);
+        const examsCollection = db.collection(constants.EXAMS_COLLECTION_NAME);
+
+        let filter = { userId: userId };
+        if (submittedAtFrom && submittedAtTo) {
+            filter.submittedAt = {
+                $gte: new Date(submittedAtFrom),
+                $lt: new Date(submittedAtTo)
+            };
+        }
+        const results = await examResultsCollection.find(filter).toArray();
+        const examIds = results.map(result => result.examId);
+        let examQuery = {_id: { $in: examIds } };
+        if (name) {
+            examQuery.name = { $regex: new RegExp(name, 'i') };
+        }
+        if (type) {
+            if (type === 'Free access') {
+                examQuery.start = null;
+            } else if (type === 'Specific time') {
+                examQuery.start = { $ne: null };
+            }
+        }
+        const exams = await examsCollection.find(examQuery).toArray();
+        let examResults = results.map(result => {
+            let exam = exams.find(exam => exam._id.equals(result.examId));
+            delete exam._id;
+            delete exam.questions;
+            delete result.userId;
+            delete result.examId;
+            const resultId = result._id;
+            delete result._id;
+            delete result.details;
+            return {
+                id: resultId,
+                exam: exam,
+                result: result
+            };
+        });
+        res.status(200).send({
+            examResults: examResults
+        });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+}
+
+async function listExamResultsByExamId(req, res) {
+    try {
+        const examId = req.query.examId;
+        console.log(examId);
+        const {submittedAtFrom, submittedAtTo} = req.query;
+
+        const {db, client} = await connectDb();
+        const examResultsCollection = db.collection(constants.EXAM_RESULTS_COLLECTION_NAME);
+        const examsCollection = db.collection(constants.EXAMS_COLLECTION_NAME);
+        const usersCollection = db.collection(constants.USERS_COLLECTION_NAME);
+
+        let filter = { examId: new ObjectId(examId) };
+        if (submittedAtFrom && submittedAtTo) {
+            filter.submittedAt = {
+                $gte: new Date(submittedAtFrom),
+                $lt: new Date(submittedAtTo)
+            };
+        }
+        const results = await examResultsCollection.find(filter).toArray();
+        const userIds = results.map(result => result.userId);
+        const users = await usersCollection.find({ _id: { $in: userIds } }).toArray();
+        const exam = await examsCollection.findOne({ _id: new ObjectId(examId) });
+        let examResults = results.map(result => {
+            let user = users.find(user => user._id === result.userId);
+            const resultId = result._id;
+            delete result._id;
+            delete result.details;
+            return {
+                id: resultId,
+                full_name: user.full_name,
+                result: result
+            };
+        });
+        res.status(200).send({
+            examResults: examResults
+        });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+}
--- routes/admin.exam.route.js
+++ routes/admin.exam.route.js
@@ -350,4 +350,122 @@ adminExamRoute.put("/:id", jwtMiddleware.validateToken, validateUtils.isAdmin, a
  */
 adminExamRoute.delete("/:id", jwtMiddleware.validateToken, validateUtils.isAdmin, adminExamController.deleteExam);
 
+/**
+ * @swagger
+ * /admin/exams/results:
+ *   get:
+ *     summary: List exam results by user ID or Exam ID
+ *     tags: [Admin Exam]
+ *     parameters:
+ *       - name: userId
+ *         in: query
+ *         required: false
+ *         description: The user ID to find exam results for
+ *         schema:
+ *           type: string
+ *         example: f44f7b65-3bc2-410e-a3bd-80d6c1de8c36
+ *       - name: examId
+ *         in: query
+ *         required: false
+ *         description: The exam ID to find exam results for
+ *         schema:
+ *           type: string
+ *         example: 6619ef8ffc3384ad4a3ab95f
+ *       - name: name
+ *         in: query
+ *         required: false
+ *         description: Filter by exam name (case insensitive, partial matches allowed)
+ *         schema:
+ *           type: string
+ *       - name: type
+ *         in: query
+ *         required: false
+ *         description: Filter by exam access type (e.g., 'Free access', 'Specific time')
+ *         schema:
+ *           type: string
+ *           enum:
+ *             - Free access
+ *             - Specific time
+ *       - name: submittedAtFrom
+ *         in: query
+ *         required: false
+ *         description: Lower boundary for submission date filter (inclusive)
+ *         schema:
+ *           type: string
+ *           format: date-time
+ *       - name: submittedAtTo
+ *         in: query
+ *         required: false
+ *         description: Upper boundary for submission date filter (exclusive)
+ *         schema:
+ *           type: string
+ *           format: date-time
+ *     responses:
+ *       200:
+ *         description: A list of exam results for the specified user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     description: The ID of the exam result
+ *                   exam:
+ *                     type: object
+ *                     properties:
+ *                       name:
+ *                         type: string
+ *                       start:
+ *                         type: string
+ *                         format: date-time
+ *                         nullable: true
+ *                       duration:
+ *                         type: integer
+ *                         description: Duration of the exam in minutes
+ *                   result:
+ *                     type: object
+ *                     properties:
+ *                       submittedAt:
+ *                         type: string
+ *                         format: date-time
+ *                       correctCount:
+ *                         type: integer
+ *                       totalQuestions:
+ *                         type: integer
+ *                       details:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             question:
+ *                               type: string
+ *                             yourAnswer:
+ *                               type: integer
+ *                             correctAnswer:
+ *                               type: integer
+ *                             isCorrect:
+ *                               type: boolean
+ *             examples:
+ *               application/json:
+ *                 - id: "661d4c265acfe5d69cc23860"
+ *                   exam:
+ *                     name: "Practice for Midterm Exam"
+ *                     start: null
+ *                     duration: 60
+ *                   result:
+ *                     submittedAt: "2024-04-15T15:47:50.353Z"
+ *                     correctCount: 10
+ *                     totalQuestions: 10
+ *       403:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: "Access is forbidden. This is only for admin"
+ */
+adminExamRoute.get("/results/", jwtMiddleware.validateToken, validateUtils.isAdmin, adminExamController.listExamResults);
+
 module.exports = adminExamRoute;
